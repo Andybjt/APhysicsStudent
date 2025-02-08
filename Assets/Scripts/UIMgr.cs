0a79fb7fa44c1f04935ad525a87fb006e8b9bf86
Andybjt <baijingtiansh@sina.com>
2025-02-08 11:45:24 +0800

    Feat; GameMgr & UIMgr

--- Assets/Scripts/UIMgr.cs
+++ Assets/Scripts/UIMgr.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class UIMgr : MonoBehaviour
 {
+    public static UIMgr I;
+
+    private void Awake()
+    {
+        I = this;
+
+
+    }
     // Start is called before the first frame update
     void Start()
     {
